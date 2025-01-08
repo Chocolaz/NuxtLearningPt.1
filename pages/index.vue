462148eac0be5d3046e8723f6fc85803d9f455d0
ศิระกาญ กาจนวิลาสกุล <sirakran.kran@mail.kmutt.ac.th>
2025-01-08 14:33:43 +0700

    composables and plugins

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <template>
-  <div class="Main">Main Page</div>
-  <img src="@/assets/img/1.jpg" alt="Minion" />
+  <div class="container py-3 mx-auto Main">
+    <h1 class="text-teal-200">Main Page</h1>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { $sayHello } = useNuxtApp()
+$sayHello('Chocolate')
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container py-3 mx-auto Profile">
+    <h1 class="text-teal-500">Profile Page</h1>
+  </div>
+</template>
+
+<script setup>
+const sayHello = () => console.log('Goodbye')
+sayHello()
+</script>
