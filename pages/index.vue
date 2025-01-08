85544160b9142e939c532a52f12191b432102496
ศิระกาญ กาจนวิลาสกุล <sirakran.kran@mail.kmutt.ac.th>
2025-01-08 16:12:55 +0700

    pinia

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
-<script setup lang="ts">
-const counter = useCounter()
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from '~/stores/myStore'
+
+const store = useCounterStore()
+const counter = ref(store.count)
+
+const increment = () => {
+  store.increment()
+  counter.value = store.count
+}
+
+const decrement = () => {
+  store.decrement()
+  counter.value = store.count
+}
+
+console.log(store)
 </script>
 
 <template>
   <div>
     <div id="main">
       Counter: {{ counter }}
-      <button @click="counter++">+</button>
-      <button @click="counter--">-</button>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
     </div>
     <Counter id="counter" />
   </div>
